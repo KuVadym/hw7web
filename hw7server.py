96d80c085bd158760625545ef2c1bf02c7e27f76
KuVadym <97878308+KuVadym@users.noreply.github.com>
2022-09-02 16:14:21 +0300

    Update hw7server.py

--- hw7server.py
+++ hw7server.py
@@ -5,13 +5,15 @@ from concurrent import futures as cf
 def message(conn, addr):
     while True:
         for address in clients:
-            data = conn.recv(1024)
-            if not data:
-                break
-            if address != conn:
-                print(f'from <{addr}> message: {data}')
-                msg = data.decode()
-                address.send((f'from <{addr}> message: {msg}').encode())
+            if address==conn:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                for i in range(len(clients)):
+                    if clients[i] != conn:
+                        print(f'from <{addr}> message: {data}')
+                        msg = data.decode()
+                        clients[i].send((f'from <{addr}> message: {msg}').encode())
     #conn.close()
 
 host = ''
@@ -21,18 +23,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))  #  Привязывает сокет к адресу address (инициализирует IP-адрес и порт). Сокет не должен быть привязан до этого.
 s.listen(10)           #  Переводит сервер в режим приема соединений. Параметр``backlog (int)`` – количество соединений, которые будет принимать сервер.
 print('server start')
-#with cf.ThreadPoolExecutor(10) as client_pool:
 while True:
     conn, addr = s.accept()
     print(f'<{addr}> conected')
     clients.append(conn)
     threading.Thread(target=message, args=[conn, addr]).start()
-    #client_pool.submit(message, conn, addr)
-    #conn.send(b'I see your message')
 
 s.close()
-
-
-# cd C:\Users\kuzik\Desktop\Vadym
-# python hw7.py
-# python hw7c.py
